94831dedb04a12bcbf1bc44f107bd8b33f7f2fdf
Evgenus <chernyshov.eugene@gmail.com>
2014-05-10 20:56:04 +0300

    revisions rendering

--- yggdrasil/app/pages/revisions.py
+++ yggdrasil/app/pages/revisions.py
@@ -1,11 +1,27 @@
 from . import Page
+from werkzeug.exceptions import NotFound
+from yggdrasil.node import (
+    RevisionId, 
+    BranchId,
+    )
 
 class Revisions(Page):
     def __init__(self, runtime):
-        pass
+        self.runtime = runtime
 
-    def on_revision_by_id(self): 
-        pass
+    def on_revision_by_id(self, request, rid): 
+        rid = RevisionId.from_string(rid)
+        revision = self.runtime.get_revision(rid)
+        if revision is None:
+            raise NotFound()
+        result = dict(
+            rid=revision.id,
+            ancestors=revision._ancestors,
+            nodes=list(revision._nodes),
+            refs=list(revision._refs.keys()),
+            finished=revision._finished,
+            )
+        return result
 
     def on_all_revision_nodes(self): 
         pass
