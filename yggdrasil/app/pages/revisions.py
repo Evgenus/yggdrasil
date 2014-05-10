94b5582b67e224c9efa2fccd97777cfb1d1b6d0f
Evgenus <chernyshov.eugene@gmail.com>
2014-05-10 21:48:25 +0300

    refs in revisions

--- yggdrasil/app/pages/revisions.py
+++ yggdrasil/app/pages/revisions.py
@@ -14,12 +14,33 @@ class Revisions(Page):
         revision = self.runtime.get_revision(rid)
         if revision is None:
             raise NotFound()
+        nodes = list(revision._nodes)
+        node_refs = list(revision._refs.keys())
+
+        refs = {
+            "ancestors": {},
+            "nodes": {},
+            "nodes_refs": {},
+        }
+        refs["branch"] = self.build_url(request, 
+                "branches.branch_by_id", bid=rid.branch)
+        for number, rid in enumerate(revision._ancestors):
+            refs["ancestors"][number] = self.build_url(request, 
+                "revisions.revision_by_id", rid=rid)
+        for number, uid in enumerate(nodes):
+            refs["nodes"][number] = self.build_url(request, 
+                "nodes.by_uid", uid=uid)
+        for number, uid in enumerate(node_refs):
+            refs["nodes_refs"][number] = self.build_url(request, 
+                "nodes.by_uid", uid=uid)
+
         result = dict(
             rid=revision.id,
             ancestors=revision._ancestors,
-            nodes=list(revision._nodes),
-            refs=list(revision._refs.keys()),
+            nodes=nodes,
+            nodes_refs=node_refs,
             finished=revision._finished,
+            refs=refs,
             )
         return result
 
