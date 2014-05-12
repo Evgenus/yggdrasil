379f4657bc0e80a844e56cbd64aad034d3474f62
Evgenus <chernyshov.eugene@gmail.com>
2014-05-12 18:27:12 +0300

    refs for nodes

--- yggdrasil/app/pages/nodes.py
+++ yggdrasil/app/pages/nodes.py
@@ -1,5 +1,7 @@
+from werkzeug.exceptions import NotFound
+
 from yggdrasil.record import Record
-from yggdrasil.node import NodeId
+from yggdrasil.node import NodeId, NodeRef
 
 from . import Page
 
@@ -8,14 +10,35 @@ class Nodes(Page):
         self.runtime = runtime
         self.show_refs = show_refs
 
+    def inspect(self, request, revision, value):
+        if isinstance(value, NodeRef):
+            uid = NodeId(value, revision.search(value).id)
+            return self.build_url(request, "nodes.by_uid", uid=uid)
+        elif isinstance(value, tuple):
+            result = Record()
+            for number, subvalue in enumerate(value):
+                subinspect = self.inspect(request, revision, subvalue)
+                if subinspect is not None:
+                    result[number] = subinspect
+            if result: return result
+
     def on_by_uid(self, request, uid):
         uid = NodeId.from_string(uid)
         node = self.runtime.get_node(uid)
         if node is None:
             raise NotFound()
 
-        request = Record()
-        request.ref = node.ref
-        request.data = node.content
-        return request
-        
+        result = Record()
+        result.ref = node.ref
+        result.data = node.content
+
+        revision = self.runtime.get_revision(uid.revision)
+
+        if self.show_refs:
+            result.refs = refs = Record()
+            for name, value in node.content.items():
+                inspected = self.inspect(request, revision, value)
+                if inspected is not None:
+                    refs[name] = inspected
+
+        return result
